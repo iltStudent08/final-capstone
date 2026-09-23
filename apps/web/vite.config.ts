17ab80dc47982f2eebf3246de49527cc1f541d27
suyas <suyash.tiwari@libertymutual.com>
2026-09-23 10:35:29 -0700

    chore: proxy local api requests through vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,5 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/app/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:4001',
+    },
+  },
   plugins: [react()],
 })
